134f88b30519f8653c0f084cbd1fc071c36d0eb8
Norton|李驰 <lichi@gznb.com>
2017-11-28 10:56:00 +0800

    update router

--- router.js
+++ router.js
@@ -2,7 +2,7 @@ var express = require('express');
 var region = require('./controllers/region');
 var router = express.Router();
 
-router.get('/index', function (req, res) {
+router.get('/', function (req, res) {
     res.render('index', {
         title: "省份"
     })
